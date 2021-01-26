d9dc05fa38c4ccbb7f1ab7b1e79b83465bd9d2f2
devanshu <devbbhatt27@gmail.com>
2021-01-26 17:35:42 +0530

    Basic Structure
    Created the basic structure needed to manage the project and set up necessary modules in index file

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyp = require('body-parser');
+const path = require('path');
+
+// creating instance of Express
+const app = express();
+
+// Getting confidential/hidden values from config.env files
+dotenv.config({ "path": "./config.env"});
+
+// getting port value from env config file
+const PORT = process.env.PORT || 3000;
+
+// To see log requests
+app.use(morgan('tiny'));
+
+// parsing requests to body parser
+app.use(bodyp.urlencoded( { extended : true } ));
+
+// setting view engine
+app.use("view engine", "ejs");
+//app.set("views", path.resolve(__dirname,"views/ejs"));
+
+// loading the assets needed
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+
+
+// What we want to see on the page
+app.get('/',(req,res) => {
+    res.send('HELLO THERE!');
+})
+
+// setting our port for server
+app.listen(PORT,()=>{
+    console.log(`Server on ${PORT}`);
+});
